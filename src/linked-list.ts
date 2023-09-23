713b39b40fcac76e5dead80bac414e9c680b3957
Saad T <tariqs26@mcmaster.ca>
2023-09-23 19:17:26 -0400

    add pop at contains

--- src/linked-list.ts
+++ src/linked-list.ts
@@ -1,4 +1,4 @@
-export class ListNode {
+class ListNode {
   val: number
   next: ListNode | null
   constructor(val: number, next: ListNode | null = null) {
@@ -25,6 +25,43 @@ export class LinkedList {
 
   prepend(val: number): void {
     this.head = new ListNode(val, this.head)
+  }
+
+  pop(): number {
+    if (!this.head) throw new Error("Cannot pop from empty list")
+    let current = this.head
+    while (current.next) {
+      if (!current.next.next) {
+        const val = current.next.val
+        current.next = null
+        return val
+      }
+      current = current.next
+    }
+    const val = this.head.val
+    this.head = null
+    return val
+  }
+
+  at(index: number) {
+    if (index < 0 || index >= this.size())
+      throw new Error("Index out of bounds")
+    let current = this.head
+    let i = 0
+    while (current) {
+      if (i == index) return current.val
+      current = current.next
+      i++
+    }
+  }
+
+  contains(val: number): boolean {
+    let current = this.head
+    while (current) {
+      if (current.val === val) return true
+      current = current.next
+    }
+    return false
   }
 
   size(): number {
--- test/linked-list.test.ts
+++ test/linked-list.test.ts
@@ -1,4 +1,4 @@
-import { describe, test, beforeEach } from "node:test"
+import { describe, it, beforeEach } from "node:test"
 import { LinkedList } from "../src/linked-list"
 import assert from "node:assert"
 
@@ -8,14 +8,72 @@ describe("LinkedList module", () => {
   beforeEach(() => {
     list = new LinkedList()
   })
-  test("append", () => {
+  it("append", () => {
     list.append(4)
     assert.strictEqual(list.size(), 1)
+    assert.strictEqual(list.head?.val, 4)
   })
-
-  test("clear", () => {
+  it("prepend", () => {
+    list.append(4)
+    list.prepend(5)
+    assert.strictEqual(list.head?.val, 5)
+  })
+  describe("at", () => {
+    it("index out of bounds", () => {
+      assert.throws(
+        () => {
+          list.at(0)
+        },
+        {
+          message: "Index out of bounds",
+        }
+      )
+    })
+    it("valid index", () => {
+      list.append(4)
+      assert.deepEqual(list.at(0), 4)
+    })
+  })
+  describe("pop", () => {
+    it("Empty list", () => {
+      assert.throws(
+        () => {
+          list.pop()
+        },
+        {
+          message: "Cannot pop from empty list",
+        }
+      )
+    })
+    it("Single length list", () => {
+      list.append(4)
+      const val = list.pop()
+      assert.strictEqual(list.size(), 0)
+      assert.strictEqual(val, 4)
+    })
+    it("Multi-length list", () => {
+      list.append(4)
+      list.append(5)
+      let val = list.pop()
+      assert.strictEqual(list.size(), 1)
+      assert.strictEqual(val, 5)
+      val = list.pop()
+      assert.strictEqual(list.size(), 0)
+      assert.strictEqual(val, 4)
+    })
+  })
+  it("clear", () => {
     list.append(4)
     list.clear()
     assert.strictEqual(list.head, null)
+  })
+  describe("toString", () => {
+    it("Empty list", () => {
+      assert.strictEqual(list.toString(), "null")
+    })
+    it("Non-empty list", () => {
+      list.append(4)
+      assert.strictEqual(list.toString(), "4 -> null")
+    })
   })
 })
