ae33d2f9c7848b20af2cf1b19c33bad65d3a5fc1
Saad T <tariqs26@mcmaster.ca>
2023-09-23 19:17:09 -0400

    convert to iife

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import { LinkedList } from "./linked-list"
 
-function main() {
+;(function main() {
   const list = new LinkedList()
   list.append(4)
   console.log(list.toString())
-}
-
-main()
+})()
