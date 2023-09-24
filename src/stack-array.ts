fc00dc806c1dcd47621a844e58864cf998ca0f81
Saad Tariq <saadtariq.dev@outlook.com>
2023-09-23 20:04:11 -0400

    add implementation

--- src/stack-array.ts
+++ src/stack-array.ts
@@ -1,6 +1,37 @@
 /**
  * Stack implementation using array
  */
-class Stack<T> {
 
+class EmptyStackError extends Error {
+  constructor() {
+    super("Error: Stack is empty")
+  }
+}
+export class Stack<T> {
+  data: T[]
+  constructor() {
+    this.data = []
+  }
+
+  push(val: T) {
+    this.data.push(val)
+  }
+
+  pop() {
+    if (!this.data.length) throw new EmptyStackError()
+    return this.data.pop()
+  }
+
+  peek() {
+    if (!this.data.length) throw new EmptyStackError()
+    return this.data.at(this.data.length - 1)
+  }
+
+  size() {
+    return this.data.length
+  }
+
+  clear() {
+    this.data = []
+  }
 }
