a8beaca3605b9af289085facf6fe989e2f466383
Saad Tariq <saadtariq.dev@outlook.com>
2023-09-24 13:20:12 -0400

    add size and search

--- src/binary-tree.ts
+++ src/binary-tree.ts
@@ -17,3 +17,13 @@ export function depth(root: TreeNode<unknown> | null): number {
   if (!root) return 0
   return 1 + Math.max(depth(root.right), depth(root.left))
 }
+
+export function size(root: TreeNode<unknown> | null): number {
+  if (!root) return 0
+  return 1 + size(root.left) + size(root.right)
+}
+
+export function search<T>(root: TreeNode<T> | null, val: T): boolean {
+  if (!root) return false
+  return root.val === val || search(root.left, val) || search(root.right, val)
+}
