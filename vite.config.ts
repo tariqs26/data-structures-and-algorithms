9ba8d585bf697b6d1837cd8877b622b47f289e8f
Saad Tariq <saadtariq.dev@outlook.com>
2024-02-12 16:54:58 -0500

    test: update coverage exclude paths

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,14 @@ import react from "@vitejs/plugin-react-swc"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {},
+  test: {
+    coverage: {
+      exclude: [
+        "*.config.{js,ts}",
+        ".eslintrc.cjs",
+        "src/types/**",
+        "src/vite-env.d.ts",
+      ],
+    },
+  },
 })
