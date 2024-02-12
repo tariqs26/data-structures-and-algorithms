927c0e64f270f744fd5a61881e377413a16a632a
Saad Tariq <saadtariq.dev@outlook.com>
2024-02-12 16:59:11 -0500

    ci: update base path for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react-swc"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/data-structures-and-algorithms/",
   plugins: [react()],
   test: {
     coverage: {
